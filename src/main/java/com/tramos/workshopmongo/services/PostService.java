018a6fda95080044f1031fac93a1859f6b78bc94
Tiago Maia Ramos <tramos@mpmg.mp.br>
2026-06-11 19:32:09 -0300

    Consulta simples com query methods

--- src/main/java/com/tramos/workshopmongo/services/PostService.java
+++ src/main/java/com/tramos/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.tramos.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,10 @@ public class PostService {
     public Post findById(String id) {
         return repo.findById(id)
             .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 
 }
