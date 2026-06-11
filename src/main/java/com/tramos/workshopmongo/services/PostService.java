2c60a0f8ca883b0a085caeb53a5d4fae65f0612b
Tiago Maia Ramos <tramos@mpmg.mp.br>
2026-06-11 17:45:40 -0300

    Obtendo um post por id

---
+++ src/main/java/com/tramos/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.tramos.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tramos.workshopmongo.domain.Post;
+import com.tramos.workshopmongo.repository.PostRepository;
+import com.tramos.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+    
+    public Post findById(String id) {
+        return repo.findById(id)
+            .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
